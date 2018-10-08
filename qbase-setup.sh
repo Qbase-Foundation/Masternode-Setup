4762d60b85d9d37665cb19fca303e252e4a1951a
Qbase-Crypto <43824063+Qbase-Crypto@users.noreply.github.com>
2018-10-08 15:02:12 -0400

    Update qbase-setup.sh

--- qbase-setup.sh
+++ qbase-setup.sh
@@ -189,8 +189,8 @@ maxconnections=64
 externalip=$publicip
 masternode=1
 masternodeprivkey=$genkey
-addnode=207.246.82.215:13964
-addnode=45.76.56.178:13964
+addnode=seed1.qbs.signal2noi.se:13964
+addnode=seed2.qbs.signal2noi.se:13964
 EOF
 
 #Finally, starting qbase daemon with new qbase.conf
